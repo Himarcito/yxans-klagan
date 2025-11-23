74be66836c47b8a974560ab57f8c2f9f568bbe86
Himarcito <himarcito@gmail.com>
2025-11-23 18:47:12 +0000

    Update translation.data.ts

--- src/store/translations/translation.data.ts
+++ src/store/translations/translation.data.ts
@@ -1,7 +1,8 @@
 import { en } from '../../data/translations/en'
+// ¡Importante! Asegúrate de que NO hay línea que diga 'import { sv }'
 
 const translationFiles = {
-  en,
+  en, // Solo cargamos el inglés
 }
 
 export const loadTranslations = async (lang: string) => {
