e9f37ac24202d36c256966ab31d7531030ca7de5
Himarcito <himarcito@gmail.com>
2026-02-20 22:28:24 +0000

    Update monster.model.ts

--- src/features/monsters/monster.model.ts
+++ src/features/monsters/monster.model.ts
@@ -219,7 +219,6 @@ export type MonsterMotivation =
 
 export type MonsterAttackType = any
 
-// Aquí están los nombres corregidos para que coincidan con tu diccionario en camelCase
 const generateMonsterAttacks = () => {
   const dict: Record<string, string> = {
     slash: 'monsters:attack.slash.type',
@@ -302,7 +301,8 @@ const generateMonsterAttacks = () => {
     'giant_specter', 'giant_spider_hatchling', 'giant_spider_male', 'giant_spider_female',
     'greater_golem', 'imp', 'iron_dragon', 'mara', 'mire_drake', 'mummy',
     'nature_spirit', 'possessor', 'rat_king', 'rock_troll', 'shapeshifter',
-    'skolopendra', 'swarming_death'
+    'skolopendra', 'swarming_death', 
+    'tupilaq', 'twisted_ent', 'undead_dragon', 'vampyr', 'water_troll', 'will_o_the_wisp', 'wolfshadow'
   ]
 
   for (const m of monsters) {
